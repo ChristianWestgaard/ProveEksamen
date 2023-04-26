88703b90a4ea6319354ade48c309a0c20a8088e4
Christian Westgaard <westgaard04@gmail.com>
2023-04-26 17:39:02 +0200

    IDK

--- app.js
+++ app.js
@@ -28,3 +28,80 @@ app.get('/logInn', function(req, res){
 
 app.listen(8080);
 console.log('Server is listening on port 8080');
+
+
+const  { MongoClient } = require('mongodb')
+
+let admin = 0
+
+async function main() {
+
+   const uri= 'mongodb+srv://christianwestgaard04:Admin1@store.rrovops.mongodb.net/?retryWrites=true&w=majority';
+   const client = new MongoClient(uri);
+
+  //  logInn(client)
+
+   try{
+      // await client.connect(); 
+
+      // createUser(client, {
+      //    name: document.getElementById("suUsername"),
+      //    password: document.getElementById("suPassword"),
+      //    email: document.getElementById("suEmail")
+      // })    
+      
+   } catch (e) {
+      console.log(e);
+   } finally {
+      await client.close();
+   }
+}
+
+main().catch(console.error);
+
+async function createUser(client, newListing) {
+   const result = await client.db("proveeksamen").collection("users").insertOne(newListing);
+
+   console.log('New listing created with ID:'+ result);
+}
+
+// function logInn(client){
+//   if (document.getElementById("liUsername") === client.db("proveeksamen").collection("users").findOne(document.getElementById("liUsername")) 
+//     && document.getElementById("liPassword") === client.db("proveeksamen").collection("users").findOne(document.getElementById("liPassword"))){
+//       admin = 1
+//       console.log("admin"+admin);
+//   }
+// }
+ 
+
+function addShoe(){
+  document.getElementById()
+}
+
+async function listDatabases(client){
+   const dbList = await client.db().admin().listDatabases();
+
+   console.log('Databases');
+   dbList.databases.forEach(db => {
+      console.log('- ' + db.name);
+   })
+}
+
+function getUser(){
+
+
+  let findResult = users.find({
+    username: document.getElementById("liUsername").value
+  })
+}
+
+function logInn() {
+  alert("CONTACT")
+}
+
+document.getElementById("liB").value.onclick = logInn()
+
+
+
+
+
